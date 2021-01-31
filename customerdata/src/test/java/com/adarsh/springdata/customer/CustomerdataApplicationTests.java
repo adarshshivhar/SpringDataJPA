999ae15f64d33ce62d8b9367fd140fbce509fc52
Adarsh Shivhare <adarshshivhare789@gmail.com>
2021-02-01 00:42:01 +0530

    Initial Commit

---
+++ customerdata/src/test/java/com/adarsh/springdata/customer/CustomerdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adarsh.springdata.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
